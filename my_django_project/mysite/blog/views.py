f1678e0033b480aa3ad2345f6f98b3fd4540631a
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-12 13:35:40 +0300

    created forms

--- my_django_project/mysite/blog/views.py
+++ my_django_project/mysite/blog/views.py
@@ -7,3 +7,8 @@ def frontpage(request):
     posts = Post.objects.all()
 
     return render(request, 'blog/frontpage.html',{'posts':posts})
+
+def post_detail(request,slug):
+    post = Post.objects.get(slug = slug)
+
+    return render(request,'blog/post_detail.html',{'post':post})
