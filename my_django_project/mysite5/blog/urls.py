90b1e4d71fed85f2c4d19d38e1a96c7a08632ccb
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-17 01:35:42 +0300

    updated blogs/urls

--- my_django_project/mysite5/blog/urls.py
+++ my_django_project/mysite5/blog/urls.py
@@ -1,1 +1,10 @@
 #blogs/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path('',views.revenue,name='revenue'),
+    path('',views.Customer_Analysis,name='Customer_Analysis'),
+    path('',views.Custmer_Behaviour,name='Custmer_Behaviour'),
+]
