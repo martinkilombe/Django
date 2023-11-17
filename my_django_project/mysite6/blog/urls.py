13378b8fc0565ddb3f0e053c2296cbe7ee478f0b
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-17 17:48:53 +0300

    New changes to iframe links

--- my_django_project/mysite6/blog/urls.py
+++ my_django_project/mysite6/blog/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns=[
     path('revenue-analysis/',views.Revenue_Analysis,name='Revenue_Analysis'),
     path('customer-behaviour-analysis/',views.Customer_Behaviour,name='Customer_Behaviour'),
-    path('account-analysis',views.Account_Analysis,name='Account_Analysis'),
+    path('account-analysis/',views.Account_Analysis,name='Account_Analysis'),
 ]
