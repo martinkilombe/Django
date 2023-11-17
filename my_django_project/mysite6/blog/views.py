197aa99dc0f85db402e2294e66684a620720bb11
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-17 16:31:36 +0300

    updated blog views

--- my_django_project/mysite6/blog/views.py
+++ my_django_project/mysite6/blog/views.py
@@ -1,3 +1,16 @@
+#blog/views
 from django.shortcuts import render
 
 # Create your views here.
+#Directs paths to the revenue Analysis
+def Revenue_Analysis(request):
+    return render(request,'blog\Revenue_Analysis.html')
+
+#Customer Behaviour
+def Customer_Behaviour(request):
+    return render(request,'blog\Customer_Behaviour.html')
+
+
+#Account Analysis
+def Account_Analysis(request):
+    return render(request,'blog\Account_Analysis.html')
