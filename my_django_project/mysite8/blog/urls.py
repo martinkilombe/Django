b3c78a78575c094c34bcbbff63992d457df14053
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-21 18:23:55 +0300

    final Url and Model updates

--- my_django_project/mysite8/blog/urls.py
+++ my_django_project/mysite8/blog/urls.py
@@ -1,11 +1,12 @@
 #blogs/urls.py
 from django.urls import path
-from .views import Revenue_Analysis,Account_Analysis,Customer_Behaviour
+from .views import Revenue_Analysis,Account_Analysis,Customer_Behaviour,Product_Analysis
 
 urlpatterns=[
     path('revenue-analysis/',Revenue_Analysis,name='Revenue_Analysis'),
     path('customer-behaviur-analysis/',Customer_Behaviour,name='Customer_Behaviour'),
     path('account-analysis/',Account_Analysis,name='Account_Analysis'),
+    path('product-analysis/',Product_Analysis,name='Product_Analysis'),
 
     #Add other URL patterns
 ]
