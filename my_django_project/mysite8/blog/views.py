b1b33936489ae26ec761efe32af07e1bfccb1070
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-11-21 12:37:47 +0300

    Updated views - Product_Analysis

--- my_django_project/mysite8/blog/views.py
+++ my_django_project/mysite8/blog/views.py
@@ -66,3 +66,25 @@ def Account_Analysis(request):
 
     # Render a template with the iframe URL
     return render(request, 'blog/Account_Analysis.html', {'iframe_url': iframe_url})
+
+
+#Produuct Analysis
+def Product_Analysis(request):
+    METABASE_SITE_URL = "http://localhost:3000"
+    METABASE_SECRET_KEY = "b3dd70e957bd04a868d85e812ca5322c9e1ae09026dabc12e48211fe74e0056b"
+
+    # Define the payload for the JWT token
+    payload = {
+    "resource": {"dashboard": 36},
+    "params": {},
+    "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+    }
+
+    # Generate the JWT token
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+
+     # Construct the iframe URL
+    iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true"
+
+    #Render a template with the iframe URL
+    return render(request,'blog/Product_Analysis.html',{'iframe_url':iframeUrl})
