4c30a2830ff0ecb077105296ea9c5336811bc484
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-12-13 22:18:13 +0300

    updated htmls

--- my_django_project/mysite9/blog/models.py
+++ my_django_project/mysite9/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#blog/models.py
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
