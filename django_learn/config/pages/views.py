b75e8e9f43a1739f876a728706b4882a2aaf325c
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2023-12-17 23:07:00 +0300

    created tests

--- django_learn/config/pages/views.py
+++ django_learn/config/pages/views.py
@@ -1,8 +1,16 @@
 #pages/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Freeze all motor functions')
+#Hompage view
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
+
+#About Page view
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+    
