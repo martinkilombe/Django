babd094e0de0bc597d545f8495c81977b0cebcb7
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2024-02-03 22:38:07 +0300

    updated config.urls

--- django_learn/blog/config/urls.py
+++ django_learn/blog/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('accounts/',include('django.contrib.auth.rls')),
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
 ]
