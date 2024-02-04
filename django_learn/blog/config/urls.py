50994ef5722c6021b18f83b6636c1f2652307eb3
Martin Muti Kilombe <47008504+martinkilombe@users.noreply.github.com>
2024-02-04 21:11:42 +0300

    forms and user accounts

--- django_learn/blog/config/urls.py
+++ django_learn/blog/config/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('accounts/',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), 
+    path('', include('blog.urls')),
 ]
+
